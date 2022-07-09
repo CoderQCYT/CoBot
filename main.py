089d31b2e18fde9996620d76268075c55cd53fdf
CoderQC <73763394+CoderQCFromScraatch@users.noreply.github.com>
2022-07-08 20:33:46 -0700

    fixes

--- main.py
+++ main.py
@@ -12,26 +12,39 @@ mem = gd.memory.get_memory()
 win = win32gui.FindWindow(None, 'Geometry Dash')
 
 def click():
-    win32api.SendMessage(win, win32con.WM_LBUTTONDOWN, win32con.MK_LBUTTON, win32api.MAKELONG(10, 10))
-
+    keyboard.press("space")
 def unclick():
-    win32api.SendMessage(win, win32con.WM_LBUTTONUP, win32con.MK_LBUTTON, win32api.MAKELONG(10, 10))
+    keyboard.release("space")
 
-accurate = 0
+def clicktwo():
+    keyboard.press("up")
+def unclicktwo():
+    keyboard.release("up")
+    
+
 recording = 1
 delay = 0.01
 speedhack = 1
+physics = 1
 step = 0
 step_setup = False
 frame = 1
+playback_ended = False
     
 while True:
     if keyboard.is_pressed('P') and recording != 0:
         recording = 0
         print("In playing mode")
+    if keyboard.is_pressed('F') and physics == 1:
+        print ("Physics is off")
+        physics = 0
     if keyboard.is_pressed('R') and recording != 1:
         recording = 1
         print("In record mode")
+    if keyboard.is_pressed('E') and physics == 0:
+        print ("Physics is on")
+        physics = 1
+
     if mem.get_x_pos() > 0 and recording == 1:
         try:
             os.remove("macro.cobot")
@@ -40,23 +53,25 @@ while True:
         print('Starting to record!')
         f = open("macro.cobot","w")
         frame = 1
-        macro = {"macro":[]}
+        # speedhack = float(input("Speedhack: "))
+        macro = {"speedhack":speedhack,"macro":[]}
         cli = 0
-        while mem.get_x_pos() > 0:
+        twopcli = 0
+        while mem.get_x_pos() > 0.1:
             if not(mem.is_dead()):
                 if step_setup == True:
                     mem.set_x_pos(step_x)
                     mem.set_y_pos(step_y)
-                    print("Tried to set!")
                     step_setup = False
-                list = {"click":cli,"x": str(mem.get_x_pos()),"y": str(mem.get_y_pos()),"frame":frame}
-                print("Frame: " + str(frame))
-                if win32api.GetKeyState(0x01) < 0:
+                list = {"click":cli,"arrow":twopcli,"x": str(mem.get_x_pos()),"y": str(mem.get_y_pos()),"frame":frame}
+                if win32api.GetKeyState(0x01) < 0 or win32api.GetKeyState(0x20) < 0:
                     cli = 1
-                    print ("Clicking: 1")
                 else:
                     cli = 0
-                    print ("Clicking: 0")
+                if win32api.GetKeyState(0x26):
+                    twopcli = 1
+                else:
+                    twopcli = 0
                 macro["macro"].append(list)
                 time.sleep(delay / speedhack)
                 frame = frame + 1
@@ -72,8 +87,7 @@ while True:
                # frame = step
                 step_setup = True
                 while frame > step:
-                    print("Deleting frame: " + str(frame))
-                    macro["macro"].pop()
+                    macro["macro"].pop(0)
                     frame = frame - 1
                     
 
@@ -87,34 +101,37 @@ while True:
         #macro = input("Enter macro name: ")
         # TODO add speedhack support 
         # p = open(macro + ".cobot")
-        print("Playing back")
-        json_file = open("macro.cobot")
-        data = json.load(json_file)
+        if not(playback_ended):
+            print("Playing back")
+            json_file = open("macro.cobot")
+            data = json.load(json_file)
+            speedhack = data['speedhack']
         # print(data)
-        while mem.get_x_pos() > 0:
+        while mem.get_x_pos() > 0.1 and mem.is_in_level():
             try:
                 if not(mem.is_dead()):
-                    print("Frame: " + str(frame))
-#                    if not(mem.get_x_pos() == float(data['macro'][frame]['x'])):
-                    mem.set_x_pos(float(data['macro'][frame]['x']))
-#                    if not(mem.get_y_pos() == float(data['macro'][frame]['y'])):
-                    mem.set_y_pos(float(data['macro'][frame]['y']))
+                    if physics == 1:
+                        mem.set_x_pos(float(data['macro'][frame]['x']))
+                        mem.set_y_pos(float(data['macro'][frame]['y']))
                     if data['macro'][frame]['click'] == 1:
-                        print("Clicking: 1")
                         click()
                     else:
-                        print("Clicking: 0")
                         unclick()
-                    time.sleep(delay)
+                    if data['macro'][frame]['arrow'] == 1:
+                        clicktwo()
+                    else:
+                        unclicktwo()
+                    time.sleep(delay/speedhack)
                     frame = frame + 1
                 if mem.is_dead() and not(mem.is_practice_mode()):
                     frame = 0
 
             except:
+                playback_ended = True
                 print("Playback ended!")
+                json_file.close()
+                unclick()
+                playback_ended = False
                 break
 
-        json_file.close()
-        unclick()
-        
 
